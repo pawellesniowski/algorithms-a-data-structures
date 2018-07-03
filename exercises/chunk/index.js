22bf1073c94b492539db8b54a84bc84f02c7b1b3
Pawel Lesniowski <pawel_lesniowski@epam.com>
2018-07-03 12:03:52 +0200

    dividing array into smoller chunks, with slice(index-start, index-after-end)

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,27 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    // Solution 1:
+    // let chunked = [];
+    // for (let element of array) {
+    //     const last = chunked[chunked.length - 1];
+    //     if(!last || last.length === size) {
+    //         chunked.push([element]);
+    //     } else {
+    //         last.push(element)
+    //     }
+    // }
+    // return chunked;
+
+    // Solution 2:
+    let chunked = [];
+    for (let i = 0; i < array.length; i += size) {
+        let chunk = array.slice(i, i+size);
+        chunked.push(chunk);
+    }
+    return chunked;
+
+}
 
 module.exports = chunk;
