67d50ff09044b95fb89111370c6feaa2bf4b889f
Pawel Lesniowski <pawel_lesniowski@epam.com>
2018-07-03 14:11:36 +0200

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,16 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+     return toBabcia(stringA) === toBabcia(stringB);
+}
 
+function toBabcia(string) {
+    return string
+        .toLowerCase()
+        .replace(/[^\w]/g, '')
+        .split('')
+        .sort()
+        .join('');
+}
 module.exports = anagrams;
